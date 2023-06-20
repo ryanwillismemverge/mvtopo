e1a2a987375ff6dce859e1a34ed7d4f125d17183
Ryan Willis <ryan.willis@memverge.com>
2023-06-20 16:27:16 +0000

    reformat & error handling

--- mvtopo.py
+++ mvtopo.py
@@ -45,17 +45,36 @@ def main():
     
 def generate_topology(manual_links: list|None) -> dict:
     graph = {}
-    generate_numa_nodes(graph)
-    generate_dax_devices(graph)
-    generate_cxl_devices(graph)
-    generate_socket_devices(graph)
-    generate_root_devices(graph) 
+    try:
+        generate_numa_nodes(graph)
+    except Exception:
+        print("Error: Couldn't generate NUMA nodes.")
+    try:
+        generate_dax_devices(graph)
+    except Exception:
+        print("Error: Couldn't generate DAX devices.")
+    try:
+        generate_cxl_devices(graph)
+    except Exception:
+        print("Error: Couldn't generate DAX devices.")
+    try:
+        generate_socket_devices(graph)
+    except Exception:
+        print("Error: Couldn't generate socket devices.")
+    try:
+        generate_root_devices(graph) 
+    except Exception:
+        print("Error: Couldn't generate root devices.")
     generate_mem_dax_links(graph)
-    generate_memory_topology(graph)
+    try:
+        generate_memory_topology(graph)
+    except Exception:
+        print("Error: Couldn't generate DAX devices.")
     generate_manual_links(graph, manual_links)
     return graph
 
 def format_topology(manual_links: list|None):
+    manual_links = [] if manual_links is None else manual_links
     if manual_links is None:
         manual_links = []
     def recurse(nodes, node_id, link_types):
@@ -81,9 +100,6 @@ def format_topology(manual_links: list|None):
             recurse(topology, id, set())
             
     return new_topology
-
-import os
-import re
 
 def generate_numa_nodes(graph: dict):
     node_dir = "/sys/devices/system/node/"
@@ -117,7 +133,6 @@ def generate_numa_nodes(graph: dict):
 def generate_dax_devices(graph: dict):
     dax_dir = "/sys/bus/dax/devices/"
     devices = {}
-
     for item in os.listdir(dax_dir):
         target_node_file = os.path.join(dax_dir, item, "target_node")
         numa_node_file = os.path.join(dax_dir, item, "numa_node")
@@ -150,13 +165,11 @@ def generate_cxl_devices(graph: dict):
                 break
         except Exception as e:
             exceptions.append(e)
-            
     if success:
         graph.update(graph_copy)
     else:
         raise Exception(f'Couldn\'t generate CXL devices: {str(exceptions)}')
     
-
 def generate_cxl_devices_cxlctl(graph: dict):
     for memdev in cxl_list_memdevs():
         device_health = memdev['health'] if 'health' in memdev else None
@@ -168,16 +181,17 @@ def generate_cxl_devices_cxlctl(graph: dict):
             'device_ram_size': memdev['ram_size'] // (1024 * 1024),
             'health': device_health
         }
-        device.update(parse_lstopo_output(memdev['memdev']))
+        try:
+            device.update(parse_lstopo_output(memdev['memdev']))
+        except Exception as e:
+            print('Error: Couldn\'t interpret lstopo output.')
         graph[memdev['memdev']] = device
     return True
-
 
 def generate_cxl_devices_sysfs(graph: dict) -> int:
     def get_memdev_ram_size_sysfs(memdev):
         with open(f"/sys/bus/cxl/devices/{memdev}/ram/size", "r") as file:
             return int(file.read().strip(), 16) // (1024 * 1024)
-        
     def get_memdev_serial_sysfs(memdev_name):
         file_path = f"/sys/bus/cxl/devices/{memdev_name}/serial"
         with open(file_path, "r") as file:
@@ -195,47 +209,12 @@ def generate_cxl_devices_sysfs(graph: dict) -> int:
                 'device_ram_size': get_memdev_ram_size_sysfs(item),
                 'health': None
             }
-            memdev.update(parse_lstopo_output(item))
+            try:
+                memdev.update(parse_lstopo_output(memdev['memdev']))
+            except Exception as e:
+                print('Error: Couldn\'t interpret lstopo output.')
             graph[item] =  memdev
     return True
-            
-
-
-def find_lstopo_parents(device):
-    result = subprocess.run(['lstopo-no-graphics', '-v'], stdout=subprocess.PIPE)
-    lines = result.stdout.decode('utf-8').split('\n')
-    
-    tree = {}
-    level = {}
-    last_node_at_level = {}
-    devices = {}
-    full_lines = {}
-    pattern = re.compile(r'^(\s*)(.*?)( \(.*?\))?(\s*\"(.*?)\")?$')   # Matches leading spaces, trims the ending description in brackets, and extracts device names
-
-    for line in lines:
-        match = pattern.match(line)
-        if match:
-            depth = len(match.group(1))    # The number of leading spaces is the depth level
-            node = match.group(2).strip()  # Trim any leading or trailing spaces
-            device_name = match.group(5)   # Extract device name
-            if device_name:                # If device name was found
-                devices[device_name] = node    # Map device name to full node name
-            if node:                       # Exclude empty lines
-                full_lines[node] = line    # Keep track of the full line for each node
-                parent = last_node_at_level.get(depth - 2, None)  # Parent is the last node at depth-2 (because each depth level represents two spaces)
-                tree[node] = parent
-                level[node] = depth
-                last_node_at_level[depth] = node
-    parents = []
-    current_node = devices.get(device, None)
-    if current_node is None:
-        raise Exception(f"Device {device} not found in lstopo output.")
-    while current_node is not None:
-        parents.append(full_lines[current_node])
-        current_node = tree[current_node]
-    parents.reverse()
-    
-    return parents
 
 def generate_root_devices(graph: dict) -> tuple:
     root_devices = {}
@@ -331,24 +310,53 @@ def generate_manual_links(graph, manual_links):
             graph[link_to]['parent'].append(link_from)
         except Exception as e:
             print(f'Couldn\'t manually link {link_from} -> {link_to}')
-
+            
+def find_lstopo_parents(device):
+    result = subprocess.run(['lstopo-no-graphics', '-v'], stdout=subprocess.PIPE)
+    lines = result.stdout.decode('utf-8').split('\n')
+    tree = {}
+    level = {}
+    last_node_at_level = {}
+    devices = {}
+    full_lines = {}
+    pattern = re.compile(r'^(\s*)(.*?)( \(.*?\))?(\s*\"(.*?)\")?$')   # Matches leading spaces, trims the ending description in brackets, and extracts device name
+    for line in lines:
+        match = pattern.match(line)
+        if match:
+            depth = len(match.group(1))
+            node = match.group(2).strip()
+            device_name = match.group(5)
+            if device_name:
+                devices[device_name] = node
+            if node:
+                full_lines[node] = line
+                parent = last_node_at_level.get(depth - 2, None)
+                tree[node] = parent
+                level[node] = depth
+                last_node_at_level[depth] = node
+    parents = []
+    current_node = devices.get(device, None)
+    if current_node is None:
+        raise Exception(f"Device {device} not found in lstopo output.")
+    while current_node is not None:
+        parents.append(full_lines[current_node])
+        current_node = tree[current_node]
+    parents.reverse()
+    return parents
 
 def find_socket_nodes():
     output = subprocess.check_output(['numactl', '-H'], universal_newlines=True)
-
     node_pattern = re.compile(r'node\s+(\d+)\s+cpus:\s+([\d\s]+)')
     nodes = {}
     for match in node_pattern.finditer(output):
         node_id = match.group(1)
         cpus = [int(cpu) for cpu in match.group(2).split()]
         nodes[f'node{node_id}'] = cpus
-
     return nodes
 
 def get_memory_info():
     cmd_output = subprocess.check_output("sudo dmidecode -t memory", shell=True).decode('utf-8')
     memory_info = cmd_output.split("\n")
-
     modules = []
     module_info = {}
     handle = None
@@ -372,9 +380,7 @@ def get_memory_info():
             modules.append(module_info)
             module_info = {}
             handle = None
-    
     return modules
-
 
 def list_cxl_devices() -> list:
     out = subprocess.getoutput('ls /sys/bus/cxl/devices/')
@@ -400,10 +406,7 @@ def get_numa_node_size(node_name: str) -> int:
             
 def get_cpu_info():
     cpu_info_dict = {}
-
-    # Run the lscpu command and split the output into lines
     lscpu_output = subprocess.check_output("lscpu", shell=True).decode('utf-8').split("\n")
-    
     for line in lscpu_output:
         if "Architecture:" in line:
             cpu_info_dict["Architecture"] = line.split(":")[1].strip()
@@ -447,10 +450,8 @@ def parse_lstopo_output(memdev):
 def get_socket_nodes():
     command = 'lstopo-no-graphics'
     output = subprocess.getoutput(command)
-
     socket_nodes = {}
     current_socket = None
-
     for line in output.split('\n'):
         if 'Package L#' in line:
             current_socket = line.split('Package L#')[1].split()[0]
@@ -459,7 +460,6 @@ def get_socket_nodes():
         elif 'NUMANode L#' in line:
             node_num = line.split('P#')[1].split()[0]
             socket_nodes[socket_key].append(f'node{node_num}')
-
     return socket_nodes
 
 def get_node_cpus(node_name):
@@ -467,7 +467,6 @@ def get_node_cpus(node_name):
     numactl_output = subprocess.getoutput('numactl -H')
     numactl_lines = numactl_output.split('\n')
     cpus_list = []
-
     for line in numactl_lines:
         if line.startswith(f"node {node_id} cpus"):
             cpus_list = line.split(':')[1].strip().split(' ')
@@ -477,16 +476,13 @@ def get_node_cpus(node_name):
 def parse_cpu_list(online_cpu_list):
     cpus = []
     ranges = online_cpu_list.split(',')
-
     for cpu_range in ranges:
         if '-' in cpu_range:
             start, end = map(int, cpu_range.split('-'))
             cpus.extend(range(start, end + 1))
         else:
             cpus.append(int(cpu_range))
-
     return cpus
-
                         
 if __name__ == "__main__":
     main()
